f13770a84791534138b8deb11591e5e13205660a
Barbara Mayfield <bmayfield1991@gmail.com>
2019-10-30 19:41:29 -0500

    Have first successfully passing test in Display.spec.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,4 +1,9 @@
-// Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Display from './Display';
+import Controls from '../controls/Controls'
 
 // ### Display Component
 
@@ -7,3 +12,27 @@
 // - displays 'Locked' if the `locked` prop is `true` and 'Unlocked' if otherwise
 // - when `locked` or `closed` use the `red-led` class
 // - when `unlocked` or `open` use the `green-led` class
+
+// Test away!
+
+afterEach(rtl.cleanup);
+
+test("Display component gate open/closed, locked/unlocked", async() => {
+    const wrapperDisplay = rtl.render(<Display />);
+    const wrapperControls = rtl.render(<Controls/>);
+    const locked = wrapperDisplay.getByText(/locked/i)
+    const unlocked = wrapperDisplay.getByText(/unlocked/i)
+    const open = wrapperDisplay.getByText(/open/i)
+    const closed = wrapperDisplay.getByText(/close/i)
+    
+    expect(unlocked).toBeInTheDocument();
+    expect(open).toBeInTheDocument();
+    
+    rtl.act(() => {
+        rtl.fireEvent.click(wrapperControls.queryByText(/Lock Gate/i));
+        rtl.fireEvent.click(wrapperControls.queryByText(/Close Gate/i));
+    })
+
+    expect(locked).toBeVisible()
+    expect(closed).toBeVisible()
+})
